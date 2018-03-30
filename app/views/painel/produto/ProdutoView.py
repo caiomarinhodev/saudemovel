7cdd90d487ab2142fa19a72ea874a2998cea2e5c
Caio Marinho <caiomarinho8@gmail.com>
2018-03-30 19:51:54 -0300

    remove prints from produtoview

--- app/views/painel/produto/ProdutoView.py
+++ app/views/painel/produto/ProdutoView.py
@@ -1,12 +1,9 @@
-from base64 import b64encode
-
-import pyimgur
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from app.forms import FormProduto, GrupoFormSet, FotoProdutoFormSet, GrupoUpdateFormSet, FotoProdutoUpdateFormSet
-from app.models import Produto, Estabelecimento, FotoProduto
+from app.models import Produto, Estabelecimento
 from app.views.mixins.Mixin import FocusMixin
 from app.views.script_tools import logger
 
@@ -47,7 +44,6 @@ class ProdutoCreateView(LoginRequiredMixin, CreateView, FocusMixin):
         except (Exception,):
             pass
         context = self.get_context_data()
-        print(context)
         gruposet = context['gruposet']
         fotoset = context['fotoset']
         with transaction.atomic():
@@ -58,19 +54,9 @@ class ProdutoCreateView(LoginRequiredMixin, CreateView, FocusMixin):
             if fotoset.is_valid():
                 fotoset.instance = self.object
                 fotoset.save()
-        # message = "Um novo pedido foi feito pela " + self.request.user.first_name
-        # print('>>>>>>>> Novo Pedido criado pela loja ' + self.request.user.first_name)
-        # pedido = self.object
-        # if not pedido.is_draft:
-        #     a = func()
-        #     for m in Motorista.objects.all():
-        #         if m.is_online and not m.ocupado:
-        #             n = Notification(type_message='NOVO_PEDIDO', to=m.user, message=message)
-        #             n.save()
         return super(ProdutoCreateView, self).form_valid(form)
 
     def form_invalid(self, form):
-        print(form.errors)
         return super(ProdutoCreateView, self).form_invalid(form)
 
 
@@ -98,7 +84,6 @@ class ProdutoUpdateView(LoginRequiredMixin, UpdateView, FocusMixin):
         except (Exception,):
             pass
         context = self.get_context_data()
-        print(context)
         gruposet = context['gruposet']
         fotoset = context['fotoset']
         with transaction.atomic():
@@ -107,24 +92,11 @@ class ProdutoUpdateView(LoginRequiredMixin, UpdateView, FocusMixin):
                 gruposet.instance = self.object
                 gruposet.save()
             if fotoset.is_valid():
-                # for form in fotoset.cleaned_data:
-                #     if 'file' in form:
-                #         if form['file']:
                 fotoset.instance = self.object
                 fotoset.save()
-        # message = "Um novo pedido foi feito pela " + self.request.user.first_name
-        # print('>>>>>>>> Novo Pedido criado pela loja ' + self.request.user.first_name)
-        # pedido = self.object
-        # if not pedido.is_draft:
-        #     a = func()
-        #     for m in Motorista.objects.all():
-        #         if m.is_online and not m.ocupado:
-        #             n = Notification(type_message='NOVO_PEDIDO', to=m.user, message=message)
-        #             n.save()
         return super(ProdutoUpdateView, self).form_valid(form)
 
     def form_invalid(self, form):
-        print(form.errors)
         return super(ProdutoUpdateView, self).form_invalid(form)
 
 
