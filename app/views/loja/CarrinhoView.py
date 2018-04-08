4a6c361b79286223e7b7b8a2d4c496cba4fcb87b
Caio Marinho <caiomarinho8@gmail.com>
2018-04-08 12:54:59 -0300

    Fix message

--- app/views/loja/CarrinhoView.py
+++ app/views/loja/CarrinhoView.py
@@ -133,6 +133,7 @@ class FinalizaRequest(LoginRequiredMixin, TemplateView, LojaFocusMixin):
 
 def submit_pedido(request):
     data = request.POST
+    endereco = None
     try:
         cliente = request.user.cliente
         pedido = Request.objects.get(id=request.session['pedido'])
@@ -141,7 +142,6 @@ def submit_pedido(request):
         messages.error(request, 'Faça Login para finalizar o pedido')
         return redirect('/define/login/')
     try:
-        endereco = None
         if 'endereco' in data:
             if data['endereco'] != '':
                 endereco = Endereco.objects.get(id=data['endereco'])
@@ -153,6 +153,9 @@ def submit_pedido(request):
                     endereco = Endereco(cliente=cliente, bairro=bairro, endereco=data['rua'], numero=data['numero'],
                                         complemento=data['complemento'])
                     endereco.save()
+                else:
+                    messages.error(request, u'Nao conseguimos cadastrar seu endereco')
+                    return redirect('/finaliza-pedido/')
             else:
                 messages.error(request, u'Selecione o endereco de entrega ou Informe o endereco de entrega')
                 return redirect('/finaliza-pedido/')
